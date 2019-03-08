d9c6f61e957b5836e35a32cde52724574d12157a
祐太郎萩原 <s21161062@gmail.com>
2019-03-08 12:25:41 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 萩原祐太郎 on 2019/03/08.
+//  Copyright © 2019 Yutaro_Hagiwara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
